56362c450626be6f76f5889fe97d6cbe9321e76c
Vadim <mihailovvadick1996@yandex.ru>
2022-12-13 11:08:13 +0300

    create task01

--- Seminars_questions/task01/task01.py
+++ Seminars_questions/task01/task01.py
@@ -9,5 +9,10 @@
 # - 8, 9 -> нет
 
 
-num1 = int(input("num1 = "))
-num2 = int(input("num2 = "))
+num1 = int(input("Введите первое число = "))
+num2 = int(input("Введите второе число = "))
+
+if num1 == num2**2 or num2 == num1**2:
+    print('Да')
+else:
+    print('Нет')
