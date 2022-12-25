f30fbb6775035db576d32b0c774d620bd681258e
Vadim <mihailovvadick1996@yandex.ru>
2022-12-25 10:22:55 +0300

    change task 5 in DZ_Questions

--- DZ_Questions/Seminar_03/task05.py
+++ DZ_Questions/Seminar_03/task05.py
@@ -2,40 +2,45 @@
 # Пример:
 # для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] Негафибоначчи
 
-fib1 = 0
-fib2 = 1
+
 
 dig_fib = int(input('Введите число Фибоначчи: '))
 
-my_list = []
-my_list.append(fib1)
-my_list.append(fib2)
-for i in range(2, dig_fib + 1):
-    fib1, fib2 = fib2, fib1 + fib2
+def nego_fibonacci(x):
+    fib1 = 0
+    fib2 = 1
+    my_list = []
+    my_list.append(fib1)
     my_list.append(fib2)
-print(my_list)
+    for i in range(2, x + 1):
+        fib1, fib2 = fib2, fib1 + fib2
+        my_list.append(fib2)
+    print(my_list)
 
-fib_1 = 0
-fib_2 = 1
-new_list = []
-new_list.append(fib_2)
-for i in range(2, dig_fib + 1):
-    fib_1, fib_2 = fib_2, fib_1 - fib_2
+    fib_1 = 0
+    fib_2 = 1
+    new_list = []
     new_list.append(fib_2)
-print(new_list)
-new_list.reverse()
+    for i in range(2, x + 1):
+        fib_1, fib_2 = fib_2, fib_1 - fib_2
+        new_list.append(fib_2)
+    print(new_list)
+    new_list.reverse()
 
-new_list2 = []
+    new_list2 = []
 
-for i in range(len(new_list)):
-    new_list2.append(new_list[i])
-
-
-for i in range(len(my_list)):
-    new_list2.append(my_list[i])
+    for i in range(len(new_list)):
+        new_list2.append(new_list[i])
 
 
-print(f'для k = 8 список будет выглядеть так: {new_list2} Негафибоначчи')
+    for i in range(len(my_list)):
+        new_list2.append(my_list[i])
+
+    return new_list2
+
+
+result = nego_fibonacci(dig_fib)
+print(f'для k = 8 список будет выглядеть так: {result} Негафибоначчи')
 
 
 
