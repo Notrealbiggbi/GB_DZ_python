4f1f02bc3406290e0521845f2bf77621fdb6daf1
Vadim <mihailovvadick1996@yandex.ru>
2023-01-19 12:15:09 +0300

    change task01

--- DZ_Questions/Seminar_05/task01.py
+++ DZ_Questions/Seminar_05/task01.py
@@ -2,14 +2,14 @@ from random import randint as RT
 
 
 my_candy = RT(29, 101)
-print(f'Это игра в "Кто взял последнюю конфету тот и проиграл" \nВот кол-во конфет {my_candy}')
+print(f'Это игра в "Кто взял последнюю конфету тот и выиграл" \nВот кол-во конфет {my_candy}')
 tu = "Первый Игрок"
 
 for _ in range(my_candy):
 
     if my_candy != 0:
         print(f'\n Ход: {tu}\n')
-        turn = int(input(' Введи сколько конфет хочешь взять: '))
+        turn = int(input(' Введи сколько конфет хочешь взять но не больше 28: '))
         res = my_candy - turn
         my_candy = res
         print(f'\nКонфет осталось: {res}\n')
@@ -19,4 +19,9 @@ for _ in range(my_candy):
             tu = "Первый Игрок"
     else:
         break
+if tu == "Первый Игрок":
+    tu = "Второй Игрок"
+else:
+    tu = "Первый Игрок"
+
 print(f"Игра окончена!!! Выиграл {tu}")
