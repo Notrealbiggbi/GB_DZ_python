813bbc4509c3d4f6b4e5e98107db04090ef656f5
Vadim <mihailovvadick1996@yandex.ru>
2023-01-19 13:36:53 +0300

    change task01

--- DZ_Questions/Seminar_05/task01.py
+++ DZ_Questions/Seminar_05/task01.py
@@ -1,34 +1,51 @@
-from random import randint as RT
+from random import randint as RI
 
 
-my_candy = RT(29, 101)
-print(f'Это игра в "Кто взял последнюю конфету тот и выиграл" \nВот кол-во конфет {my_candy}')
-tu = "Первый Игрок"
+my_candy = 150
+take_candy = 0
+player_candy = 0
+bot_candy = 0
 
-for _ in range(my_candy):
+def start_game():
+    print("Игра началась всего на столе лежит 150 конфет бери \n"
+          "конфеты пока их не останется но брать можно не больше 28")
+    who_is_first()
 
-    if my_candy != 0:
-        print(f'\n Ход: {tu}\n')
-        turn = int(input(' Введи сколько конфет хочешь взять но не больше 28: '))
-        if turn <= 28:
-            res = my_candy - turn
-            my_candy = res
-            print(f'\nКонфет осталось: {res}\n')
-            if tu == "Первый Игрок":
-                tu = "Второй Игрок"
-            else:
-                tu = "Первый Игрок"
-        else:
-            print(' ')
-            print('Не больше 28')
-            print(' ')
-
-
+def who_is_first():
+    random_number = RI(1, 2)
+    if random_number == 1:
+        player_turn()
     else:
-        break
-if tu == "Первый Игрок":
-    tu = "Второй Игрок"
-else:
-    tu = "Первый Игрок"
+        bot_turn()
 
-print(f"Игра окончена!!! Выиграл {tu}")
+
+def player_turn():
+    global my_candy
+    global take_candy
+    global player_candy
+    print(f"Твой ход. Конфет {my_candy}")
+    take_candy = int(input("Сколько конфет возьмёшь?: "))
+    while take_candy > 28 or take_candy < 0 or take_candy > my_candy:
+        take_candy = int(input("Слишком ного взял. Сколько конфет возьмёшь? "))
+    my_candy -= take_candy
+    player_candy += take_candy
+    if my_candy > 0:
+        bot_turn()
+    else:
+        print("Победа!!")
+
+def bot_turn():
+    global take_candy
+    global my_candy
+    global bot_candy
+    take_candy = my_candy % 29 if my_candy % 29 != 0 else RI(1, 28)
+    my_candy -= take_candy
+    bot_candy += take_candy
+    print(f"Бот взял {take_candy} конфет! Осталось {my_candy} конфет")
+    if my_candy > 0:
+        player_turn()
+    else:
+        print("Победа БОТА!!")
+
+
+start_game()
