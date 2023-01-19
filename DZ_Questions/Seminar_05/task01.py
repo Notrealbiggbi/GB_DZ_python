260368a158c1b47db4ee133442909935ab2487ce
Vadim <mihailovvadick1996@yandex.ru>
2023-01-19 12:28:50 +0300

    change task01

--- DZ_Questions/Seminar_05/task01.py
+++ DZ_Questions/Seminar_05/task01.py
@@ -10,13 +10,20 @@ for _ in range(my_candy):
     if my_candy != 0:
         print(f'\n Ход: {tu}\n')
         turn = int(input(' Введи сколько конфет хочешь взять но не больше 28: '))
-        res = my_candy - turn
-        my_candy = res
-        print(f'\nКонфет осталось: {res}\n')
-        if tu == "Первый Игрок":
-            tu = "Второй Игрок"
+        if turn <= 28:
+            res = my_candy - turn
+            my_candy = res
+            print(f'\nКонфет осталось: {res}\n')
+            if tu == "Первый Игрок":
+                tu = "Второй Игрок"
+            else:
+                tu = "Первый Игрок"
         else:
-            tu = "Первый Игрок"
+            print(' ')
+            print('Не больше 28')
+            print(' ')
+
+
     else:
         break
 if tu == "Первый Игрок":
