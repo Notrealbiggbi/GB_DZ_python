a4da980cbb381319659f4b173d41a14aa1246236
Vadim <mihailovvadick1996@yandex.ru>
2023-02-05 18:07:54 +0300

    create tast file

--- DZ_Questions/Seminar_08/test.py
+++ DZ_Questions/Seminar_08/test.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 def class_marks():
     excel_data = pd.read_excel('6B.xlsx')
-    data = pd.DataFrame(excel_data, columns=['id', 'Русский язык', "Английский", "География"], )
+    data = pd.DataFrame(excel_data, columns=['id', 'Русский язык', "Английский",
+                                             "География", 'Математика', 'Астрономия'])
     print("Итоговые оценки 6B:\n", data)
 
 
 def class_marks2():
     excel_data2 = pd.read_excel('5A.xlsx')
-    data2 = pd.DataFrame(excel_data2, columns=['id', 'Русский язык', "Английский", "География"], )
+    data2 = pd.DataFrame(excel_data2, columns=['id', 'Русский язык', "Английский",
+                                               "География", 'Математика', 'Астрономия'])
     print("Итоговые оценки 5A:\n", data2)
 
