8cb6a358560bb994c2c241793c2dcff5fe26131a
Vadim <mihailovvadick1996@yandex.ru>
2023-02-06 00:04:04 +0300

    create tast file

--- DZ_Questions/Seminar_08/test.py
+++ DZ_Questions/Seminar_08/test.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from openpyxl import load_workbook
+
+
 
 def class_marks():
     excel_data = pd.read_excel('6B.xlsx')
@@ -14,13 +15,4 @@ def class_marks2():
                                                "География", 'Математика', 'Астрономия'])
     print("Итоговые оценки 5A:\n", data2)
 
-# excel_data2 = pd.read_excel('5A.xlsx')
-# data2 = pd.DataFrame(excel_data2, columns=['id', 'Русский язык', "Английский",
-#                                            "География", 'Математика', 'Астрономия'])
-# print("Итоговые оценки 5A:\n", data2)
-#
-# wb = load_workbook('5A.xlsx')
-# sheet = wb.get_sheet_names('Sheet1')
-#
-# for i in range(1, 2):
-#      print(i, sheet.cell(row=i, column=2).value)
+
