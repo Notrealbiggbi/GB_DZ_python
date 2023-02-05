89d26dc46b9ee828ba7ede399bce103051e39f8d
Vadim <mihailovvadick1996@yandex.ru>
2023-02-05 17:45:21 +0300

    create tast file

--- DZ_Questions/Seminar_08/test.py
+++ DZ_Questions/Seminar_08/test.py
@@ -1,19 +1,12 @@
 import pandas as pd
-#
-#
-# def class_marks():
-#     excel_data = pd.read_excel('6B.xlsx')
-#     data = pd.DataFrame(excel_data, columns=['id', 'Русский язык', "Английский", "География"], )
-#     print("Оценки 6B:\n", data)
-#     for i in data:
-#         print(data[i])
+
+
+def class_marks():
+    excel_data = pd.read_excel('6B.xlsx')
+    data = pd.DataFrame(excel_data, columns=['id', 'Русский язык', "Английский", "География"], )
+    print("Оценки 6B:\n", data)
 
 
 
 
-excel_data = pd.read_excel('6B.xlsx')
-data = pd.DataFrame(excel_data, columns=['id', 'Русский язык', "Английский", "География", 'Математика', 'Астрономия'])
-print("Оценки 6B:\n", data)
-for i in data:
-    print(data[i])
 
